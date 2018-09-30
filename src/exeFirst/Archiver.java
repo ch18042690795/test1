510727eb1d482271604668f5538df9d38f633d69
陈昊 <chenhao1@bonc.com.cn>
2018-09-30 18:00:04 +0800

    the first demos

--- src/exeFirst/Archiver.java
+++ src/exeFirst/Archiver.java
@@ -3,10 +3,12 @@ package exeFirst;
 /**
  * Created by BONC on 2018/9/25.
  */
-public class aaa {
+public class Archiver {
 
 
     public static void main(String[] args) {
+     //归档文件
 
     }
+
 }
---
+++ src/test/ArchiveTest.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by BONC on 2018/9/27.
+ */
+
+public class ArchiveTest {
+   //将整数转化成一个字节数组
+    @Test
+
+}
